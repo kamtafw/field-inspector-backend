b3c3aaf8553e9644536beff5b6f64749c47519bf
tiva-OG <tivasalvation@gmail.com>
2026-02-23 06:49:51 +0100

    create superuser

--- apps/inspections/admin.py
+++ apps/inspections/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import InspectionTemplate, Inspection
 
-# Register your models here.
+
+@admin.register(InspectionTemplate)
+class InspectionTemplateAdmin(admin.ModelAdmin):
+    list_display = ["name", "version", "is_active", "created_at"]
+    list_filter = ["is_active"]
+    search_fields = ["name"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+
+
+@admin.register(Inspection)
+class InspectionAdmin(admin.ModelAdmin):
+    list_display = ["facility_name", "status", "inspector", "created_at"]
+    list_filter = ["status"]
+    search_fields = ["facility_name", "facility_address"]
+    readonly_fields = ["id", "created_at", "updated_at", "version"]
